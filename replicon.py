32e58c327b663863a676276351f57c784686da77
Melvie Tarr <melvie.tarr@EmpireDS.com>
2017-10-17 15:00:07 -0700

    Added tests & new function to rep class, changed directory structure for clarity

---
+++ replicon.py
@@ -0,0 +1,121 @@
+import requests
+from requests import Session
+from requests.auth import AuthBase
+import json
+import sys
+import datetime
+
+class Replicon():
+
+    def __init__(self, companyName, userName, password):
+        self.company = companyName
+        self.session = Session()
+        self.session.auth = (self.company+'\\'+userName, password)
+        self.session.headers = {'Content-Type': 'application/json'}
+        print(self._getRepliconnectUrl())
+        resp = self.session.post(self._getRepliconnectUrl(), data=json.dumps({'Action':'BeginSession'}))
+        
+        assert(resp)
+
+
+    def _getBaseServiceUrl(self):
+        return f"https://na3.replicon.com/{self.company}/services"
+
+
+    def _getRepliconnectUrl(self):
+
+        # url = self.session.post(f'http://services.replicon.com/FetchRemoteApiUrl.ashx?CompanyKey={self.company}&Version=8.29.66')
+        return f"https://na3.replicon.com/{self.company}/RemoteAPI/RemoteAPI.ashx/8.29.66/"
+
+
+    def getUsers(self):
+        url = self._getBaseServiceUrl()  + '/UserService1.svc/GetAllUsers'
+
+        response = self.session.post(url)
+
+        return response
+
+
+    def getBulkUsers(self, userURIs):
+        url = self._getBaseServiceUrl()  + '/UserService1.svc/BulkGetUsers'
+
+        query = {
+                 "userUris": [userURIs]
+                 }
+        return self.session.post(url, data=json.dumps(query))
+
+
+    def getTimeOff(userURI, startDate, endDate):
+        url = self._getBaseServiceUrl()  + '/TimeOffService1.svc/GetTimeOffDetailsForUserAndDateRange'
+        s_year, s_month, s_day = startDate.timetuple() #Or not time tupple just tuple of dates
+        e_year, e_month, e_day = endDate.timetupe()
+        query={"userUri": userURI,
+               "dateRange": {"startDate": {"year": s_year,
+                                           "month": s_month,
+                                           "day": s_day},
+                             "endDate": {"year": e_year,
+                                         "month": e_month,
+                                         "day": e_day},
+                             "relativeDateRangeUri": None,
+                             "relativeDateRangeAsOfDate": None}
+            }
+
+        response = self.session.post(url, data=json.dumps(query))
+
+        return response
+
+
+    def getUserByEmail(self, email):
+        """TODO: FIX"""
+        url = self._getBaseServiceUrl()  + '/UserService1.svc/GetUser2'
+        url = self._getRepliconnectUrl()
+        query={}
+        query['data'] = {"Action": "Query", "DomainType":"Replicon.Domain.User",
+                         "QueryType":"UserByLoginName", "Args": [email], "SortBy":["LastName","FirstName"],
+                         }
+
+        response = self.session.post(url, data=json.dumps(query))
+
+        return response
+
+
+    def getUserByLogin(self, login):
+        url = self._getBaseServiceUrl()  + '/UserService1.svc/GetUser2'
+
+        query={"user": {"uri": None,
+                        "loginName": login,
+                        "parameterCorrelationId": None}}
+
+        response = self.session.post(url, data=json.dumps(query))
+        assert(response)
+        return response
+
+
+    def getAllHolidays(self,startDate,endDate):
+        """TODO: chanve from year to actual range"""
+        start_day, start_month, start_year = startDate
+        end_day, end_month, end_year = endDate
+
+        url = self._getBaseServiceUrl() +'/HolidayCalendarService1.svc/GetHolidaysInDateRange'
+
+        query = {"holidayCalendarUri": "urn:replicon-tenant:dynamic-structures-new:holiday-calendar:3",
+                 "dateRange": {"startDate": {"year": start_year,
+                                             "month": start_month,
+                                             "day": start_day},
+                               "endDate": {"year": end_year,
+                                           "month": end_month,
+                                           "day": end_day},
+                               "relativeDateRangeUri": None,
+                               "relativeDateRangeAsOfDate": None }
+                }
+
+        response = self.session.post(url, data=json.dumps(query))
+        return response
+
+
+    def getAllHolidays(self):
+        year = datetime.year()
+        return getHolidays((1,1,year), (31,12,year))
+
+
+
--- tests/test_wraplicon.py
+++ tests/test_wraplicon.py
@@ -1,12 +1,56 @@
-from repliwrap import Replicon
+from replicon import Replicon
 
-test_userName = ""
-test_userName = ""
-test_company = ""
-
-
+test_rep = Replicon(test_company, test_userName, test_password)
+        
 
 def test_GetUser():
-    rep = Replicon(test_userName,test_userName ,test_company)
+    resp = test_rep.getUsers()
+    assert resp.status_code == 200
+    assert resp.json()['d'][0]['loginName'] == test_userName
 
-    assert rep.getUsers().json()
+
+def test_getUserByLogin():
+    resp = test_rep.getUserByLogin(test_userName)
+    assert resp.status_code == 200
+    assert resp.json()['d'][0]['slug'] = ''
+
+
+def test_getbulkUser():
+    userURIs = ['','','']
+    resp = test_rep.getBulkUsers(userURIs) 
+
+    assert resp.status_code == 200
+    for user in userURIs:
+        assert user in resp.json()['d'][0].values()
+    
+
+def test_getTimeOff():
+    startDate = (1,1,2017)
+    endDate = (1,30,2017)
+
+    resp = test_rep.getTimeOff(startDate, endDate)
+
+    assert resp.status_code == 200
+
+
+def test_getAllHolidays():
+    holidays = ["Holiday","Family Day", "Good Friday", "Easter Monday",
+                "Victoria Day", "Canada Day", "BC Day", "Labor Day",
+                "Thanksgiving Day", "Remembrance Day",
+                "Christmas Day", "Boxing Day", "Stat"]
+    resp = test_rep.getAllHolidays()
+
+    assert resp.status_code == 200
+    for holiday in holidays:
+        assert holliday in resp.json()['d'][0].values()
+
+
+def test_getHolidays():
+    startDate = (20,12,2017)
+    endDate = (31,12,2017)
+    expected_holidays = ['Christmas Day', 'Boxing Day']
+    resp = test_rep.getHolidays(startDate, endDate)
+
+    assert resp.status_code == 200
+    for holiday in expected_holidays:
+        assert holiday in resp.json()['d'][0].values()
